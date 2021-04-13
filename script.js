ac2539f9a792ab7abfd061550315e46a5c150afe
ANGELA <angela.mbithe@student.moringaschool.com>
2021-04-13 21:05:24 +0300

    add js script

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-function akanName(); {
-    var cc = document.getElementById('century').nodeValue;
-    var yy = document.getElementById('year').nodeValue;
-    var mm = document.getElementById('month').nodeValue;
-    var dd = document.getElementById('day').nodeValue;
-    var gender = document.getElementById('gender').nodeValue;
-    var dayOfTheWeek = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )%7
+function getAkanName(){
+    let year = parseInt(document.getElementById("year").value);
+    let month = parseInt(document.getElementById("month").value);
+    let day = parseInt(document.getElementById("day").value);
+    let dateOfBirth = new Date(year + "/" + month + "/" + day);
+    let answer = dateOfBirth.getDay();
+    let output = document.getElementById("displayAkanName");
+    let gender = document.getElementById("gender");
+    let male = document.getElementById("male");
+    let female = document.getElementById("female");
+
+    const days = ["Sunday", "Monday", "Tusday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const maleNames = ["kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+    const femaleNames = ["Akosua", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"]
+
+
+ if ((year < 0) || (year > 2020)) {
+        output.innerHTML =  " please enter a valid year! "
+    } else if ((month < 1) || (month > 12)) {
+        output.innerHTML =  " please enter a valid month! "
+    } else if (day < 0 || day > 31) {
+        output.innerHTML = " please enter a valid day! "
+    }
+
+    if (gender == "male") {
+         document.getElementById("displayAkanName").innerHTML = "your Akan name is" + maleNames[answer];
+    }
+     else if (gender == "female") {
+        document.getElementById("displayAkanName").innerHTML = "your Akan name is" + femaleNames[answer];
+   }
 }
